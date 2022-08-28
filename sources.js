e71ffd2a9f1aef9fabd479109b7b10127571b319
itzickw <80103682+itzickw@users.noreply.github.com>
2022-08-28 16:43:50 +0300

    test

--- sources.js
+++ sources.js
@@ -1,5 +1,8 @@
+const PORT = process.env.PORT;
+
 const sources = {
     categories: ["חשבון", "טבע", "טריוויה"],
     colors: ["chocolate", "springgreen", "deepskyblue", "deeppink", "end"],
+    PORT,
 }
 module.exports = sources;
